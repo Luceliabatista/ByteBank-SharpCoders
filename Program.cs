738ea1c53397daccd94cf69ebe4ddf3d556db94d
Lucélia Batista <lucelia.ti@gmail.com>
2022-12-20 11:54:55 -0300

    Feat: Add methods to menu options: Registrar novo usuário and Listar todas as Contas

--- Program.cs
+++ Program.cs
@@ -1,44 +1,76 @@
-﻿namespace ByteBank {
+﻿using System;
 
-    public class Program {
-
-        static void ShowMenu() {
+namespace ByteBank
+{
+    public class Program
+    {
+        static void ShowMenu()
+        {
             Console.WriteLine("1 - Inserir novo usuário");
             Console.WriteLine("2 - Deletar um usuário");
-            Console.WriteLine("3 - Detalhes de um usuário");
-            Console.WriteLine("4 - Total armazenado no banco");
+            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("4 - Detalhes de um usuário");
+            Console.WriteLine("5 - Total armazenado no banco");
+            Console.WriteLine("6 - Manipular a conta");
             Console.WriteLine("0 - Para sair do programa");
             Console.Write("Digite a opção desejada: ");
         }
 
-        public static void Main( string[] args ) {
+        static void ListarTodasAsContas(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
+        {
+            for (int i = 0; i < cpfs.Count; ++i)
+            {
+                Console.WriteLine($"CPF = {cpfs[i]} | Titular = {titulares[i]} | Saldo = R$ {saldos[i]:F2}");
+            }
+        }
+        static void RegistrarNovoUsuario(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
+        {
+            Console.WriteLine("Digite o cpf: ");
+            cpfs.Add(Console.ReadLine());
+            Console.WriteLine("Digite o nome: ");
+            titulares.Add(Console.ReadLine());
+            Console.WriteLine("Digite a senha: ");
+            senhas.Add(Console.ReadLine());
+            saldos.Add(0);
+        }
+        public static void Main(string[] args)
+        {
 
             Console.WriteLine("Antes de começar a usar, vamos configurar alguns valores: ");
 
             Console.Write("Digite a quantidade de Usuários: ");
             int quantidadeDeUsuarios = int.Parse(Console.ReadLine());
 
+            List<string> cpfs = new List<string>();
+            List<string> titulares = new List<string>();
+            List<string> senhas = new List<string>();
+            List<double> saldos = new List<double>();
+
             int option;
 
-            do {
+            do
+            {
                 ShowMenu();
                 option = int.Parse(Console.ReadLine());
 
                 Console.WriteLine("-----------------");
 
-                switch (option) {
+                switch (option)
+                {
                     case 0:
                         Console.WriteLine("Estou encerrando o programa...");
                         break;
                     case 1:
-                        Console.WriteLine("Deveria estar inserindo um novo usario!");
+                        RegistrarNovoUsuario(cpfs, titulares, senhas, saldos);
+                        break;
+                    case 3:
+                        ListarTodasAsContas(cpfs, titulares, senhas, saldos);
                         break;
                 }
 
                 Console.WriteLine("-----------------");
 
             } while (option != 0);
-
         }
 
     }
